31390aa4c36282f0e9b21aa64e1e05c59bc0d402
Ahmd Hdyt <94432124+maddhdyt@users.noreply.github.com>
2022-10-09 13:20:56 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const toggle = document.querySelector(".nav-toggle")
+const menu = document.querySelector(".nav-menu")
+const desc_title = document.querySelectorAll(".description_title")
+const desc_text = document.querySelectorAll(".description_text")
+
+window.onscroll = function () {
+    const header = document.querySelector('.header');
+    const fixedNav = header.offsetTop;
+  
+    if (window.pageYOffset > fixedNav) {
+      header.classList.add("header-fixed")
+      menu.classList.add("nav-show");
+    } else {
+      header.classList.remove("header-fixed")
+    }
+  }
+  document.onclick = function (event) {
+    if (event.target !== menu && event.target !== toggle) {
+      menu.classList.add("nav-show");
+    }
+  }
+
+toggle.addEventListener("click", function () {
+    menu.classList.toggle("nav-show")
+    toggle.classList.toggle("change-bg")
+})
+
+desc_title[0].addEventListener("click", function () {
+    desc_title[0].classList.toggle("change-bg")
+    desc_text[0].classList.toggle("unshow")
+    desc_text[1].classList.add("unshow")
+    desc_text[2].classList.add("unshow")
+})
+desc_title[1].addEventListener("click", function () {
+    desc_title[1].classList.toggle("change-bg")
+    desc_text[1].classList.toggle("unshow")
+    desc_text[0].classList.add("unshow")
+    desc_text[2].classList.add("unshow")
+})
+desc_title[2].addEventListener("click", function () {
+    desc_title[2].classList.toggle("change-bg")
+    desc_text[2].classList.toggle("unshow")
+    desc_text[0].classList.add("unshow")
+    desc_text[1].classList.add("unshow")
+})
